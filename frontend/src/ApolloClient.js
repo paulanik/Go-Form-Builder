81cc5aabcb248f83e8c5c451d48a3c2dfbb27600
Anik Kumar Paul <19-39880-1@student.aiub.edu>
2024-09-17 10:34:44 +0600

    frontend replace

--- frontend/src/ApolloClient.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'http://localhost:8080/query', // Replace with your GraphQL endpoint
-  cache: new InMemoryCache(),
-});
-
-export default client;
